0ff89516c69408ee1e5cab69b7e9945ee75be1bb
johngachinu <166712819+johngachinu@users.noreply.github.com>
2024-05-01 19:50:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+//console.log(`icon hotel`);
+//console.log(`icon menu`);
+
+//window.alert(`boby web`);
+//window.alert(`food menu`);
+
+//document.getElementById("icon").onclick = function(){
+    //firstname = document.getElementById('icon').value;
+//const icon = document.getElementById("icon");
+//const secondname = document.getElementById("secondname");
+//const pass = document.getElementById("pass");
+//const email = document.getElementById("email");
+//const phone = document.getElementById("phone");
+//const food = document.getElementById("food");
+//const quantity = document.getElementById("quantity");
+//const Mr = document.getElementById("Mr");
+//const Mrs = document.getElementById("Mrs");
+//const phD = document.getElementById("phD");
+//const payment = document.getElementById("payment");
+//const confirm = document.getElementById("confirm");
+//confirm.prototype=function({})
+
+    //if (icon.checked) {
+        //subResult.textConten = `Your first name!`;
+    //}
+    //if (secondname.checked) {
+       // subResult.textConten = `Your secondname name!`;
+   // }
+   // if (pass.checked) {
+      //  subResult.textConten = `Your adress name!`;
+   // }
+   // if (email.checked) {
+      //  subResult.textConten = `Your email name!`;
+  //  }
+    //if (phone.checked) {
+        //subResult.textConten = `Your phone number!`;
+   // }
+    //if (food.checked) {
+       // subResult.textConten = `Your food order!`;
+   // }
+   // if (quantity.checked) {
+        //subResult.textConten = `Your quantity!`;
+   // }
+  //  if (Mr.checked) {
+        //subResult.textConten = `Your are Mr!`;
+   // }
+    //if (Mrs.checked) {
+       // subResult.textConten = `Your are Mrs!`;
+   // }
+   // if (phD.checked) {
+       // subResult.textConten = `Your are phD holder!`;
+   // }
+    //if (payment.checked) {
+      //  subResult.textConten = `Your have payed!`;
+   // }
+   // if (submit.checked) {
+      //  subResult.textConten = `confirmed!`;
+   // }
+
+
+
